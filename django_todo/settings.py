8b28b9077af9d8bd65b2cfa08593af46c4b0ff81
Chaosas100 <101866007+Chaosas100@users.noreply.github.com>
2023-09-16 21:20:54 +0100

    final push

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,13 +78,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASE_URL = os.getenv('postgres://hmdncjlg:zSK3LbXAPQk3QMNTcMOsUIjXv_NkEnV2@surus.db.elephantsql.com/hmdncjlg')
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- todo/models.py
+++ todo/models.py
@@ -9,4 +9,5 @@ class Item(models.Model):
     
     def __str__(self):
         return self.name
+        
     
