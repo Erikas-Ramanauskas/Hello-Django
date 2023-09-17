c59e41ffc68dd3a76e0a7b3f2d297d44f66ad566
Chaosas100 <101866007+Chaosas100@users.noreply.github.com>
2023-09-17 01:56:43 +0100

    replaced secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = bool(os.environ.get('SECRET_KEY')) 
+SECRET_KEY = bool(os.environ.get('SECRET_KEY','')) 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
@@ -34,7 +34,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['127.0.0.1']
 else:    
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = ['helo-django-todo-erik-96d4fb74ea67.herokuapp.com']
 
 
 # Application definition
